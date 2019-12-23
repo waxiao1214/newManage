0c5e9cc7fac51802cfc244d50dcd4d17adef95ed
Henry <Henryza1994@outlook.com>
2019-12-23 17:03:34 +0800

    test Mode

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import SimpleTestCase
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    
